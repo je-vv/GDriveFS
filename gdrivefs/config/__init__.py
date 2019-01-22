c9e4103a4e2bd032a8651e7d7088982d3d6821bc
Javier Vasquez <j.e.vasquez.v@gmail.com>
2019-01-21 19:35:25 -0600

    Fixing Multi threading issue:  https://github.com/dsoprea/GDriveFS/issues/132

--- gdrivefs/config/__init__.py
+++ gdrivefs/config/__init__.py
@@ -1,5 +1,6 @@
 import os
 
 IS_DEBUG = bool(int(os.environ.get('GD_DEBUG', '0')))
+NO_THREADS = bool(int(os.environ.get('GD_NOTHREADS', '1')))
 DO_LOG_FUSE_MESSAGES = bool(int(os.environ.get('GD_DO_LOG_FUSE_MESSAGES', '0')))
 DEFAULT_CREDENTIALS_FILEPATH = os.path.expandvars('$HOME/.gdfs/creds')
